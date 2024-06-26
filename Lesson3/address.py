3e0743fcf85f963efaa1f2219525ad5118b2bebb
Наталья Юхно <tuse4ka1984@rambler.ru>
2024-06-26 18:13:15 +0400

    HW

---
+++ Lesson3/address.py
@@ -0,0 +1,10 @@
+class Address:
+    def __init__(self, postal_code, city, street, house, apartment):
+        self.postal_code = postal_code
+        self.city = city
+        self.street = street
+        self.house = house
+        self.apartment = apartment
+
+    def __str__(self):
+        return f"{self.postal_code}, {self.city}, {self.street}, {self.house} - {self.apartment}"
---
+++ Lesson3/smartphone.py
@@ -0,0 +1,8 @@
+class Smartphone:
+    def __init__(self, brand, model, phone_number):
+        self.brand = brand
+        self.model = model
+        self.phone_number = phone_number
+
+    def __str__(self):
+        return f"{self.brand} - {self.model}. {self.phone_number}"
